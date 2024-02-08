c7a0c941a22c64f0a144b539434a0f0d9ea31d34
jnuho <jnuho@outlook.com>
2024-02-08 16:26:06 +0900

    .

---
+++ python_work/farming_stop.py
@@ -0,0 +1,53 @@
+import keyboard
+import mouse
+import time
+import pyautogui as pag
+import pygetwindow as gw
+
+from pynput.keyboard import Key, Controller
+kb = Controller()
+
+def moveto_l_click(x, y):
+  pag.moveTo(x,y)
+  mouse.press(button='left')
+  time.sleep(.2)
+  mouse.release(button='left')
+  time.sleep(.5)
+
+def pressAndRelease(key):
+    keyboard.press(key)
+    time.sleep(.2)
+    keyboard.release(key)
+    time.sleep(.4)
+
+# pyautogui의 keyboard press는 막힘
+def on_key_press(event):
+  if event.name == 'a':
+    time.sleep(1)
+
+    windows = gw.getWindowsWithTitle('Gersang')
+
+    for w in windows:
+      if w.title != 'Gersang':
+        continue
+
+      print(w)
+      w.minimize()
+      time.sleep(.5)
+      w.restore()
+      time.sleep(.5)
+
+      # game_window.activate()
+      moveto_l_click(w.left + (w.width*.2049), w.top + (w.height*.4341))
+      moveto_l_click(w.left + (w.width*.501), w.top + (w.height*.5684))
+      
+
+keyboard.on_press(on_key_press)
+
+# Keep the program running until you press the Esc key
+# keyboard.add_hotkey('ctrl+c', quit)
+# keyboard.wait(hotkey=None, suppress=False, trigger_on_release=False)
+keyboard.wait('ctrl+c')
+
+# res = pag.locateOnScreen("edit.png")
+# print(res)

455d2e03809cde96aa10fa968dd1d7acd6b18002
jnuho <cactoos555@gmail.com>
2024-02-10 21:37:54 +0900

    .

--- python_work/stay_in_sanyangteo.py
+++ python_work/stay_in_sanyangteo.py
@@ -4,8 +4,16 @@ import datetime
 import pygetwindow as gw
 
 from pynput.keyboard import Key, Controller
-kb = Controller()
 
+kb = Controller()
+count=0
+
+def on_key_press(event):
+  pass
+  # if event.name == 'esc':
+  #   print("esc")
+  # elif event.name == 'ctrl+c':
+  #   print("ctrl+c")
 
 def pressAndRelease(key):
     keyboard.press(key)
@@ -13,30 +21,38 @@ def pressAndRelease(key):
     keyboard.release(key)
     time.sleep(.4)
 
-# pyautogui의 keyboard press는 막힘
-count=0
-while True:
-  windows = gw.getWindowsWithTitle('Gersang')
+def do_work():
+  global count
+  # pyautogui의 keyboard press는 막힘
+  while True:
+    windows = gw.getWindowsWithTitle('Gersang')
 
-  for window in windows:
-    if window.title != 'Gersang':
-      continue
+    for window in windows:
+      if window.title != 'Gersang':
+        continue
 
-    print(datetime.datetime.now())
-    # if count == 0:
-    window.minimize()
-    time.sleep(.5)
-    window.restore()
-    time.sleep(.5)
-    window.activate()
-    time.sleep(.5)
-    pressAndRelease('enter')
-    pressAndRelease('1')
-    pressAndRelease('enter')
+      print(datetime.datetime.now())
+      # if count == 0:
+      window.minimize()
+      time.sleep(.5)
+      window.restore()
+      time.sleep(.5)
+      window.activate()
+      time.sleep(.5)
+      pressAndRelease('enter')
+      pressAndRelease('1')
+      pressAndRelease('enter')
 
-    time.sleep(.5)
-    count = count+1
+      time.sleep(.5)
+      count = count+1
 
-  # 1분마다 enter 1
-  time.sleep(1*60)
+    # 1분마다 enter 1
+    time.sleep(1*60)
 
+do_work()
+# keyboard.on_press(on_key_press)
+
+# Keep the program running until you press the Esc key
+# keyboard.add_hotkey('ctrl+c', quit)
+# keyboard.wait(hotkey=None, suppress=False, trigger_on_release=False)
+keyboard.wait('ctrl+c')

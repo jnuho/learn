9659a368a215cdf233a6faf8ea8bccefa9e35c19
jnuho <cactoos555@gmail.com>
2024-03-25 21:50:19 +0900

    .

--- python_work/hunt_raide.py
+++ python_work/hunt_raide.py
@@ -8,9 +8,9 @@ from pynput.keyboard import Key, Controller
 
 # GLOBAL scope
 kb = Controller()
-result = list()
-threads = []
-arrows = [Key.left, Key.up, Key.right, Key.down]
+window = None
+monster = "raide"
+# monster = "air"
 resv_attack_cnt = {
   "raide": {
     2: 2,
@@ -19,13 +19,12 @@ resv_attack_cnt = {
     4: 1,
   },
 }
-window = None
-monster = "raide"
-found = ""
+
 
 def init():
   global window
   global monster
+  # init_thread(monster)
 
   # focus on window
   window = None
@@ -36,26 +35,20 @@ def init():
       continue
     w.activate()
     window = w
-    # pag.screenshot('python_work/1.png', region=(window.left, window.top, window.width, window.height))
-
 
 def pressAndRelease(key):
 
   keyboard.press(key)
-  time.sleep(.0185)
+  time.sleep(.018)
   keyboard.release(key)
-  time.sleep(.0185)
+  time.sleep(.018)
+
 
 def debuf():
   pressAndRelease('7')
   pressAndRelease('r')
   time.sleep(.01)
-  # pressAndRelease('3')
-  # pressAndRelease('r')
-  # time.sleep(.01)
-  # pressAndRelease('6')
-  # pressAndRelease('r')
-  # time.sleep(.01)
+
 
 # pyautogui의 keyboard press는 막힘
 def on_key_press(event):
@@ -63,42 +56,60 @@ def on_key_press(event):
   global monster
   global resv_attack_cnt
 
-  if event.name == 'a':
+  # a: ,
+  # d: /
+  # w: ;
+  # s: .
+  # q: [
+  # e: ]
+  # c: \
+  # x: '
+  # if event.name == 'a':
+  if event.name == ',':
     kb.press(Key.left)
-    time.sleep(.71)
+    time.sleep(.72)
     kb.release(Key.left)
-  elif event.name == 'd':
+  # elif event.name == 'd':
+  elif event.name == '/':
     kb.press(Key.right)
-    time.sleep(.71)
+    time.sleep(.72)
     kb.release(Key.right)
-  elif event.name == 'w':
+  # elif event.name == 'w':
+  elif event.name == ';':
     kb.press(Key.up)
-    time.sleep(.71)
+    time.sleep(.72)
     kb.release(Key.up)
-  elif event.name == 's':
+  # elif event.name == 's':
+  elif event.name == '.':
     kb.press(Key.down)
-    time.sleep(.71)
+    time.sleep(.72)
     kb.release(Key.down)
 
-  # q(허영): 8r  3r  2-rc  5-rc  6-rc  4-rc  `
-  elif event.name == 'q':
-
+  # 디버프 & 이동
+  # elif event.name == 'q':
+  elif event.name == '[':
+    # 게임내 q 디퍼프: 부동+원술사
+    # 코드상 디버프: 항상
+    pressAndRelease('q')
+    time.sleep(.05)
     debuf()
 
-    # for k, v in resv_attack_cnt[monster].items():
-    #   pressAndRelease(f"{k}")
-    #   pag.click(button='right')
     pressAndRelease('`')
     mouse.press(button='right')
-    time.sleep(.01)
+    time.sleep(.015)
     mouse.release(button='right')
     time.sleep(.01)
     pressAndRelease('=')
 
-  # e(딜-예약시전): 6r LC[rrrr] 2r LC[rrr] 5r LC[rrrr] 4r LC[rrr] `
-  # dosa_sim 6r 1reee  5reeee  2reeee  4reee
-  # dosa_gak 6r 1reeee 5reeeee 2reeeee 4reeee
-  elif event.name == 'c':
+  # 보호막
+  # elif event.name == 'e':
+  elif event.name == ']':
+    pressAndRelease('9')
+    pressAndRelease('r')
+
+  # 딜-예약시전
+  # elif event.name == 'c':
+  elif event.name == '\\':
     for k, v in resv_attack_cnt[monster].items():
       pressAndRelease(f"{k}")
       pressAndRelease('r')
@@ -107,7 +118,8 @@ def on_key_press(event):
         pressAndRelease('e')
       time.sleep(0.01)
 
-  elif event.name == 'x':
+  # elif event.name == 'x':
+  elif event.name == '\'':
     keyboard.press('esc')
     time.sleep(.1)
     keyboard.release('esc')
@@ -115,6 +127,8 @@ def on_key_press(event):
     keyboard.press('esc')
     time.sleep(.1)
     keyboard.release('esc')
+
+
 
 if __name__ == "__main__":
   init()
